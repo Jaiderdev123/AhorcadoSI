b7a98259e5be613bfb74cc5dea3340f4500935a0
Jaiderdev123 <jaiderceballos@udenar.edu.co>
2025-02-24 10:51:42 -0500

    Add Estado class with initial attributes for game state management

---
+++ estado.py
@@ -0,0 +1,7 @@
+class Estado:
+    def __init__(self):
+        self.palabraoculta = ""
+        self.letrasingresadas = []
+        self.letrasposibles = 27
+        self.fallospermitidos = 8
+    
